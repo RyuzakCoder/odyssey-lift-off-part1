7727b23aaf19706c7b623685673f98f09b099414
Dairon Canel <98697567+RyuzakCoder@users.noreply.github.com>
2022-08-15 15:51:14 -0400

    feat: part completed

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Layout } from '../components';
 import { useQuery, gql } from '@apollo/client';
 import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
 
 const TRACKS = gql`
   query GetTracks {
@@ -25,15 +26,14 @@ const TRACKS = gql`
  */
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
-  console.log(loading);
-  console.log(error);
-  console.log(data);
-  if(loading) return 'loading...';
-  if(error) return 'error..';
 
   return (
     <>
-      <Layout grid>{data?.tracks}</Layout>
+      <Layout grid>
+        <QueryResult error={error} loading={loading} data={data}>
+          {data?.tracksForHome?.map(track => <TrackCard key={track.id} track={track} />)}
+        </QueryResult>
+      </Layout>
     </>
   );
 };
