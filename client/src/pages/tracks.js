3307b64efc8478aae250b6bb5a09fe1f515ce3f4
Dairon Canel <98697567+RyuzakCoder@users.noreply.github.com>
2022-08-15 10:09:00 -0400

    fix tracks

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import { Layout } from '../components';
+import { useQuery, gql } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+
+const TRACKS = gql`
+  query GetTracks {
+  tracksForHome {
+    id
+    title
+    thumbnail
+    length
+    modulesCount
+    author {
+      name
+      photo
+    }
+  }
+}
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  console.log(loading);
+  console.log(error);
+  console.log(data);
+  if(loading) return 'loading...';
+  if(error) return 'error..';
+
+  return (
+    <>
+      <Layout grid>{data?.tracks}</Layout>
+    </>
+  );
 };
 
 export default Tracks;
